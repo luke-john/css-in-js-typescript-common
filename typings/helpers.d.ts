40138712475d9691b98c7fe603ef129761a11ab1
Luke John <email@lukejohn.me>
2017-08-06 14:09:40 +1000

    chore: setup based on glamorous typings

--- typings/helpers.d.ts
+++ typings/helpers.d.ts
@@ -1,20 +1,22 @@
 type Diff<
-  T extends string,
-  U extends string
+    T extends string,
+    U extends string
 > = (
-  & { [P in T]: P }
-  & { [P in U]: never }
-  & { [x: string]: never }
+    & { [P in T]: P }
+    & { [P in U]: never }
+    & { [x: string]: never }
 )[T]
 
 export type Omit<
-  T,
-  K extends keyof T
+    T,
+    K extends keyof T
 > = Pick<
-  T,
-  Diff<keyof T, K>
+    T,
+    Diff<keyof T, K>
 >
 
 export type SingleOrArray<Properties, T extends keyof Properties> = {
-  [P in T]: Properties[P] | Array<Properties[P]>
+    [P in T]:
+        | Properties[P]
+        | Array<Properties[P]>
 }
---
+++ typings/index.d.ts
@@ -0,0 +1,21 @@
+export {
+    SVGPropertiesCompleteSingle,
+    SVGPropertiesLossy
+} from './svg-properties'
+
+export {
+    CSSPropertiesCompleteSingle,
+    CSSPropertiesLossy,
+    CSSPropertiesPseudo,
+} from './css-properties'
+
+export {
+    HTMLDomTag,
+    HTMLKey,
+    SVGDomTag,
+    SVGKey,
+} from './dom-tags'
+
+export {
+    Omit,
+} from './helpers'
